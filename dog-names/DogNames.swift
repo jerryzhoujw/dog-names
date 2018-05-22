8c3b32e4c76b7d91c30007850e09135222d70971
jerryzhoujw <jerryzhoujw@gmail.com>
2018-05-22 18:47:59 +0800

    add a public class

--- dog-names/DogNames.swift
+++ dog-names/DogNames.swift
@@ -17,3 +17,45 @@ public enum DogName: String {
 public func allDogList() -> [DogName]{
     return [.Dog1, .Dog2, .Dog3]
 }
+
+
+public class FrameworkClass
+{
+    public init()
+    {
+    }
+    
+    public func add( left:Int, right:Int ) -> Int
+    {
+        let sum = left + right
+        
+        #if DEBUG
+        print("\(left) + \(right) = \(sum)")
+        #endif
+        
+        return sum
+    }
+    
+    public func subtract( left:Int, right:Int ) -> Int
+    {
+        let remainder = left - right
+        
+        #if DEBUG
+        print("\(left) - \(right) = \(remainder)")
+        #endif
+        
+        return remainder
+    }
+    
+    public func multiply( left:Int, right:Int ) -> Int
+    {
+        let multiple = left * right
+        
+        #if DEBUG
+        print("\(left) * \(right) = \(multiple)")
+        #endif
+        
+        return multiple
+    }
+    
+} // end public class FrameworkClass
